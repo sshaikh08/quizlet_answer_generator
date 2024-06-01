d7fd7a62ce4efd76269f9114b07a5b00ad946574
sshai <sshaikh524@gmail.com>
2024-06-01 12:33:45 -0400

    Working on adding directory targeting to main script

--- my_python_tools/directory_targeting.py
+++ my_python_tools/directory_targeting.py
@@ -1,14 +1,18 @@
 from os import path, mkdir
+from pathlib import Path
 from sys import exit
 
 
-def check_for_directory(directory_path: str) -> None:
+# Code Review: Initially these functions only took strings, and the type hint indicated as much.
+#              Is there anything wrong with being able to pass a type Path object as well with the way
+#              these functions are written?
+def check_for_directory(directory_path: str | Path) -> None:
     try:
         if not path.exists(directory_path):
             raise FileNotFoundError
 
     except FileNotFoundError as fe:
-        print(f"The {directory_path} does not exist."
+        print(f"{directory_path} does not exist." # Rewrite This: 
               f"\n"
               f"Check if the enter path exists and try again")
         exit(1)
@@ -17,7 +21,7 @@ def check_for_directory(directory_path: str) -> None:
 # Code Review: Check for a better way
 #              Is this redundant? or use try/except with os.makedirs?
 #              Using os.mkdir makes more sense to me
-def check_and_create_dir(dir_path: str) -> None:
+def check_and_create_dir(dir_path: str | Path) -> None:
     if not path.exists(dir_path):
         print(f"{dir_path} does not exist."
               f"\n"
@@ -28,20 +32,21 @@ def check_and_create_dir(dir_path: str) -> None:
 if __name__ == '__main__':
 
     test_1 = False
-
     if test_1:
-
         home_directory_path = path.expanduser('~')
         documents_directory = path.join(home_directory_path, 'Documents')
 
         check_for_directory(documents_directory)
 
     test_2 = False
-
     if test_2:
-        test_folder_instance = 0
-        test_folder_main_title = 'test_folder'
+        test_2_path = Path.cwd().parent.absolute()
+        print(f"Current Working Directory: {Path.cwd()}"
+              f"\n"
+              f"Parent Directory: {str(test_2_path)}")
 
-        while True:
-            try:
+        test_3_path = Path.joinpath(test_2_path, 'test_folder')
 
+        print(f"test_3_path: {test_3_path}")
+
+        check_and_create_dir(test_3_path)
--- quizlet_answer_generator.py
+++ quizlet_answer_generator.py
@@ -1,15 +1,31 @@
+from os import path
+from pathlib import Path
 from sys import exit
 
 from my_python_tools.file_targeting import get_file_name
-from my_python_tools.directory_targeting import check_for_directory
+from my_python_tools.directory_targeting import check_for_directory, check_and_create_dir
 
 WELCOME_MESSAGE = "Welcome to the Quizlet Solutions Generator"
+home_dir_path = path.expanduser('~')
+target_main_dir = Path(path.join(home_dir_path, 'Documents'))  # Documents Directory Path
+
+quizlet_svd_dir_pth = Path.joinpath(target_main_dir, 'quizlet_saved_solutions')
 
 if __name__ == '__main__':
     print(WELCOME_MESSAGE)
 
-    check_for_directory('Documents')
+    check_for_directory(target_main_dir)  # Rewrite this: make the argument type entered stronger. Possibly write this in a
+#                                           way that one variable can be passed into multiple items.
+#                                           And for somthing like filenames and directory
+#                                           paths are stored in variables in a hierarchy according to path name.
+#                                           That way variables can be passed in to multiple functions to work in
+#                                           coordination with multiple functions called in that particular module.
+#                                           Use the library "Shelve" in rewrite.
+
+    check_for_directory(quizlet_svd_dir_pth)
 
     solutions_file_name = get_file_name()
+
+    target_file_path = path.join(target_main_dir, solutions_file_name)
 
     exit(0)  # Code Review: Is this correct usage? Trying to formally end the program
